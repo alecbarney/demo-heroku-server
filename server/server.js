5f9aa107304be7237ce276e540b0cd2e626318ce
Alec Barney <alecbarney34@gmail.com>
2022-02-23 12:06:46 -0700

    test 2

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const { response } = require('express')
+
 const express = require('express')
 const path = require('path')
 
 const app = express()
 
 app.get('/', (req, res)=>{
-response.sendFile(path.join(__dirname, '../index.html'))
+res.sendFile(path.join(__dirname, '../index.html'))
 });
 
 const port = process.env.PORT || 4005;
 
 app.listen(port, ()=>{
-    console.log(`Shitting on port ${port}`)
+    console.log(` on port ${port}`)
 });
